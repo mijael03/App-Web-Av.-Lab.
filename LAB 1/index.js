3fffa53dd50200e7e0b8be08a7d2f20e1438cc63
mijael03 <mijael.cama@tecsup.edu.pe>
2022-03-21 11:51:53 -0500

    changes for exercise 1

--- LAB 1/index.js
+++ LAB 1/index.js
@@ -1,1 +1,50 @@
-console.log("hola")
+const mayor = (a, b, c) => {
+    if (a > b) {
+        if (a > c) {
+            alert("el numero mayor es: " + a)
+        } else {
+            alert("el numero mayor es: " + c)
+        }
+    } else {
+        if (b > c) {
+            alert("el numero mayor es: " + b)
+        } else {
+            alert("el numero mayor es: " + c)
+        }
+
+    }
+}
+const multiple = (a, b) => {
+    let resto = a % b
+    if (resto === 0) {
+        return true
+    } else {
+        return false;
+    }
+}
+const exercise1 = () => {
+    const a = prompt("Dame un numero")
+    const b = prompt("Dame un numero")
+    const c = prompt("Dame un numero")
+    mayor(a, b, c);
+}
+
+const exercise2 = () => {
+    let multiplos7 = []
+    let limite = 1
+    let contador = 6
+    do {
+        contador = contador + 1
+        if (multiple(contador, 7)) {
+            if (multiple(contador, 2) || multiple(contador, 3) || multiple(contador, 5)) {
+                continue;
+            } else {
+                multiplos7.push(contador)
+                limite = limite + 1
+            }
+        }
+
+    } while (limite < 51)
+    console.log(multiplos7)
+}
+
